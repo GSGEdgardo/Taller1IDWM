03a3ab41b21261cf0074e1bec29ff450f0ac2b62
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2024-05-09 14:04:37 -0400

    fix: Fix Product Model Entities, description removed

--- Taller1/Src/Models/Product.cs
+++ Taller1/Src/Models/Product.cs
@@ -5,7 +5,6 @@ public class Product
 {
     public int Id { get; set; }
     public required string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
     public string Type { get; set; } = string.Empty;
     public required int Stock { get; set; }
     public required decimal Price { get; set; } 

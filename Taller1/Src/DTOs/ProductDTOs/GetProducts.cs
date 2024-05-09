4601b2c8a870afac49485b77b7501e6a03edc59b
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2024-05-09 14:06:03 -0400

    feat: Add Delete Product Functionality to Controller, Service, Repository and DTOs created to validate fields and improve scalability

--- Taller1/Src/DTOs/ProductDTOs/GetProducts.cs
+++ Taller1/Src/DTOs/ProductDTOs/GetProducts.cs
@@ -9,7 +9,6 @@ namespace Taller1.Src.DTOs.ProductDTOs
     {
         public int Id {get; set; }
         public string Name {get; set; } = string.Empty;
-        public string Description {get; set; } = string.Empty;
         public string Type {get; set; } = string.Empty;
         public decimal Price {get; set; }
         public int Stock {get; set; }

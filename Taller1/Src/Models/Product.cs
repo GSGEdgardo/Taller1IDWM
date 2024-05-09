7cdd01ec1cec581d708735beac7ff51a6ba06c1b
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2024-05-09 01:00:01 -0400

    feat: Add Product Model

---
+++ Taller1/Src/Models/Product.cs
@@ -0,0 +1,14 @@
+
+namespace Taller1.Src.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public required int Stock { get; set; }
+    public required decimal Price { get; set; } 
+    public string Image { get; set; } = string.Empty;
+
+}
